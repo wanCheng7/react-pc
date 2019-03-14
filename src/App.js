f6c13c076bfc2df55263b72dce2d4e864c8b3694
wancheng <wancheng@cnstrong.cn>
2019-03-14 20:05:08 +0800

    remove header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Route } from 'react-router-dom';
-import Header from './common/header';
 import Home from './pages/home';
 import Detail from './pages/detail/loadable.js';
 import Login from './pages/login';
@@ -18,7 +17,6 @@ class App extends Component {
     	<Provider store={store}>
       	<BrowserRouter>
       		<div>
-            <Header />
                 <Route path='/' exact component={Home}></Route>
                 <Route path='/login' exact component={Login}></Route>
                 {/*<Route path='/write' exact component={Write}></Route>*/}
